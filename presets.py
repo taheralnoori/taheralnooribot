5a4c4e4ce1e5862ce62d03602eb2a2817c7ebb52
taheralnoori <82815443+taheralnoori@users.noreply.github.com>
2022-05-07 01:30:54 +0300

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,5 @@
+class Presets(object):
+    PROCESS_TXT = "<code>جارٍ معالجة الرابط ..⏳</code>"
+    UPLOAD_TXT = "<b>تحميل ملفك ..</b>"
+    CAPTION_TXT = "{}\n\n<b>Developer(المطور):</b><a href='https://t.me/ta_ja199'><b> @ta_ja199 🧑🏻‍💻</b></a>"
+    THUMB_URL = "https://telegra.ph/file/f50bc1ffe8268401d7c23.jpg"
