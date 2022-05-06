38bc5759293c6e505cec888842b101c6e9209317
taheralnoori <82815443+taheralnoori@users.noreply.github.com>
2022-05-07 01:29:52 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("API_TOKEN", "")
 
     # Get from my.telegram.org
     APP_ID = int(os.environ.get("API_ID", ""))
